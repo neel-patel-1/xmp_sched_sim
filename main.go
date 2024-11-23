ddb35fd73b3873773c98aedb729018cb5accfffd
Neel Patel <neelpatelbiz@gmail.com>
2024-11-23 12:43:51 -0500

    single queue single core deterministic

--- main.go
+++ main.go
@@ -2,24 +2,40 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/epfl-dcsl/schedsim/blocks"
+	"github.com/epfl-dcsl/schedsim/engine"
 )
 
-func gen_subtask(pref_dev Device, service_time float64) SubTask {
-	return SubTask{PrefDev: pref_dev, ServiceTime: service_time}
+func single_core_deterministic(interarrival_time, service_time, duration float64) {
+	engine.InitSim()
+
+	stats := &blocks.AllKeeper{}
+	stats.SetName("Main Stats")
+	engine.InitStats(stats)
+
+	// Add generator
+	g := blocks.NewDDGenerator(interarrival_time, service_time)
+	g.SetCreator(&blocks.SimpleReqCreator{})
+
+	// Create queues
+	q := blocks.NewQueue()
+
+	// Create processors
+	p := &blocks.RTCProcessor{}
+	p.AddInQueue(q)
+	p.SetReqDrain(stats)
+	engine.RegisterActor(p)
+
+	g.AddOutQueue(q)
+
+	// Register the generator
+	engine.RegisterActor(g)
+
+	fmt.Printf("Cores:%v\tservice_time:%v\tinterarrival_rate:%v\n", 1, service_time, interarrival_time)
+	engine.Run(duration)
 }
 
 func main() {
-	// Create Subtasks
-	subtask1 := gen_subtask(CPU, 1.0)
-	subtask2 := gen_subtask(Accel, 2.0)
-	subtask3 := gen_subtask(CPU, 3.0)
-
-	// Create an XMPRequest with multiple Subtasks
-	request := XMPRequest{
-		InitTime: 0.0,
-		Subtasks: []SubTask{subtask1, subtask2, subtask3},
-	}
-
-	// Print the request to verify
-	fmt.Printf("XMPRequest: %+v\n", request)
+	single_core_deterministic(10, 10, 101)
 }
