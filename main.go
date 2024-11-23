e4d077dbbd77c446d28ac0a984f1ec70fc3e64e2
Neel Patel <neelpatelbiz@gmail.com>
2024-11-23 14:02:15 -0500

    create multi phase requests

--- main.go
+++ main.go
@@ -7,6 +7,52 @@ import (
 	"github.com/epfl-dcsl/schedsim/engine"
 )
 
+type DeviceType int
+
+const (
+	Processor DeviceType = iota
+	Accelerator_0
+	Accelerator_1
+)
+
+type Phase struct {
+	blocks.Request
+	Devices []DeviceType
+}
+
+type MultiPhaseReq struct {
+	blocks.Request
+	Phases  []Phase
+	Current int
+}
+
+type MultiPhaseReqCreator struct{}
+
+// NewRequest returns a new MultiPhaseReq
+func (m MultiPhaseReqCreator) NewRequest(serviceTime float64) engine.ReqInterface {
+	return &MultiPhaseReq{
+		Phases: []Phase{
+			{
+				Request: blocks.Request{InitTime: engine.GetTime(), ServiceTime: serviceTime},
+				Devices: []DeviceType{Processor},
+			},
+			{
+				Request: blocks.Request{InitTime: engine.GetTime(), ServiceTime: serviceTime},
+				Devices: []DeviceType{Processor, Accelerator_0},
+			},
+		},
+		Current: 0,
+	}
+}
+
+func (m *MultiPhaseReq) GetDelay() float64 {
+	return m.Phases[m.Current].GetDelay()
+}
+
+func (m *MultiPhaseReq) GetServiceTime() float64 {
+	return m.Phases[m.Current].GetServiceTime()
+}
+
 func single_core_deterministic(interarrival_time, service_time, duration float64) {
 	engine.InitSim()
 
@@ -36,6 +82,25 @@ func single_core_deterministic(interarrival_time, service_time, duration float64
 	engine.Run(duration)
 }
 
+func chained_cores_multi_phase_deterministic(interarrival_time, service_time, duration float64) {
+	engine.InitSim()
+
+	stats := &blocks.AllKeeper{}
+	stats.SetName("Main Stats")
+	engine.InitStats(stats)
+
+	// Add generator
+	g := blocks.NewDDGenerator(interarrival_time, service_time)
+	g.SetCreator(&MultiPhaseReqCreator{})
+
+	q := blocks.NewQueue()
+	g.AddOutQueue(q)
+
+	engine.RegisterActor(g)
+	engine.Run(duration)
+}
+
 func main() {
-	single_core_deterministic(10, 10, 101)
+	// single_core_deterministic(10, 10, 110)
+	chained_cores_multi_phase_deterministic(10, 10, 110)
 }
