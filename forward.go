87b3a034c02ecfd8253843c3363b04aebab061c6
Neel Patel <neelpatelbiz@gmail.com>
2024-11-25 23:12:32 -0500

    three phase generic

--- forward.go
+++ forward.go
@@ -21,6 +21,23 @@ func forwardToCentralized(outQueues []engine.QueueInterface, req *MultiPhaseReq)
 	return 0
 }
 
+// Fully Connected Network With Queue Indices Selected According to topology specified in multi_gpcore_multi_axcore_three_phase
+func forwardToCentralizedPostProcThreePhase(outQueues []engine.QueueInterface, req *MultiPhaseReq) int {
+	// re-enqueue at the centralized processor
+	return 0
+}
+
+func forwardToCentralizedPreProcThreePhase(outQueues []engine.QueueInterface, req *MultiPhaseReq) int {
+	// re-enqueue at the centralized processor
+	return 1
+}
+
+func forwardToOffloaderThreePhase(outQueues []engine.QueueInterface, req *MultiPhaseReq) int {
+	// re-enqueue at the offloading gpCore
+	outQueueIdx := req.lastGPCoreIdx + 2
+	return outQueueIdx
+}
+
 type gpCoreForwardDecisionProcedure func(p *GPCore, outQueues []engine.QueueInterface, req *MultiPhaseReq) int
 
 func tryAxCoreOutqueueThenFallback(p *GPCore, outQueues []engine.QueueInterface, req *MultiPhaseReq) int {
@@ -32,4 +49,15 @@ func tryAxCoreOutqueueThenFallback(p *GPCore, outQueues []engine.QueueInterface,
 		return 0
 	}
 	return -1
+}
+
+func blockUntilAxcoreAccepts(p *GPCore, outQueues []engine.QueueInterface, req *MultiPhaseReq) int {
+	if len(outQueues) > 1 {
+		log.Fatal("GPCore: More than one axCore is not supported")
+	}
+
+	for outQueues[0].Len() >= p.outboundMax {
+		p.Wait(p.offloadCost)
+	}
+	return 0
 }
--- main.go
+++ main.go
@@ -188,6 +188,69 @@ func multi_gpcore_multi_axcore_prefn_centralized_axfn_centralized_postfn_returnt
 
 }
 
+func multi_gpcore_multi_axcore_three_phase(duration float64, speedup float64,
+	num_cores int, num_accelerators int, axCoreQueueSize int, lambda, mu float64, genType int,
+	phase_one_ratio float64, phase_two_ratio float64, phase_three_ratio float64, axCoreForwardFunc ForwardDecisionProcedure,
+	gpCoreForwardFunc gpCoreForwardDecisionProcedure, gpCoreQueueChooseFunc QueueChooseProcedure) {
+
+	engine.InitSim()
+	stats := &blocks.AllKeeper{}
+	stats.SetName("Main Stats")
+	engine.InitStats(stats)
+
+	var g blocks.Generator
+	if genType == 0 {
+		g = blocks.NewMMRandGenerator(lambda, mu)
+	} else if genType == 1 {
+		g = blocks.NewMDRandGenerator(lambda, 1/mu)
+	} else if genType == 2 {
+		g = blocks.NewMBRandGenerator(lambda, 1, 10*(1/mu-0.9), 0.9)
+	} else if genType == 3 {
+		g = blocks.NewMBRandGenerator(lambda, 1, 1000*(1/mu-0.999), 0.999)
+	}
+	g.SetCreator(&ThreePhaseReqCreator{phase_one_ratio: phase_one_ratio, phase_two_ratio: phase_two_ratio, phase_three_ratio: phase_three_ratio})
+	q := blocks.NewQueue()
+	c_post_q := blocks.NewQueue()
+	g.AddOutQueue(q)
+
+	ax_q := blocks.NewQueue()
+
+	var post_qs = make([]engine.QueueInterface, num_cores)
+
+	for i := 0; i < num_cores; i++ {
+		gpCore := &GPCore{}
+		gpCore.outboundMax = axCoreQueueSize
+		gpCore.queueChooseFunc = gpCoreQueueChooseFunc
+		gpCore.gpCoreForwardFunc = gpCoreForwardFunc
+		gpCore.gpCoreIdx = i
+		post_qs[i] = blocks.NewQueue()
+		gpCore.AddInQueue(post_qs[i])
+		gpCore.AddOutQueue(ax_q)
+		gpCore.AddInQueue(q)
+		gpCore.SetReqDrain(stats)
+		engine.RegisterActor(gpCore)
+	}
+
+	for j := 0; j < num_accelerators; j++ {
+		axCore := &AXCore{}
+		axCore.forwardFunc = axCoreForwardFunc
+		axCore.speedup = speedup
+		axCore.AddOutQueue(c_post_q)
+		axCore.AddOutQueue(q)
+		for i := 0; i < num_cores; i++ {
+			axCore.AddOutQueue(post_qs[i])
+		}
+		axCore.AddInQueue(ax_q)
+		engine.RegisterActor(axCore)
+	}
+
+	engine.RegisterActor(g)
+
+	fmt.Printf("Cores:%d\tAccelerators:%d\tMu:%f\tLambda:%f\taxCoreQueueSize:%d\taxCoreSpeedup:%f\tgenType:%d\tphase_one_ratio:%f\tphase_two_ratio:%f\tphase_three_ratio:%f\n", num_cores, num_accelerators, mu, lambda, axCoreQueueSize, speedup, genType, phase_one_ratio, phase_two_ratio, phase_three_ratio)
+	engine.Run(duration)
+
+}
+
 func main() {
 	var topo = flag.Int("topo", 0, "topology selector")
 	var mu = flag.Float64("mu", 0.02, "mu service rate") // default 50usec
@@ -202,6 +265,14 @@ func main() {
 	var phase_two_ratio = flag.Float64("phase_two_ratio", 0.5, "phase two ratio")
 	var phase_three_ratio = flag.Float64("phase_three_ratio", 0.25, "phase three ratio")
 	var speedup = flag.Float64("speedup", 1.0, "speedup factor")
+
+	var gpcore_offload_style = flag.Int("gpcore_offload_style", 0, "gpcore offload style")
+	var axcore_notify_recipient = flag.Int("axcore_notify_recipient", 0, "axcore notify recipient")
+	var gpcore_input_queue_selector = flag.Int("gpcore_input_queue_selector", 0, "gpcore input queue selector")
+
+	var axCoreForwardFunc ForwardDecisionProcedure
+	var gpCoreForwardFunc gpCoreForwardDecisionProcedure
+	var gpCoreQueueChooseFunc QueueChooseProcedure
 
 	flag.Parse()
 	fmt.Printf("Selected topology: %v\n", *topo)
@@ -221,6 +292,46 @@ func main() {
 	}
 	if *topo == 4 {
 		multi_gpcore_multi_axcore_prefn_centralized_axfn_centralized_postfn_returntosender(*duration, *speedup, *num_cores, *num_accelerators, *bufferSize, *lambda, *mu, *genType, *phase_one_ratio, *phase_two_ratio, *phase_three_ratio)
+	}
+
+	if *gpcore_offload_style == 0 {
+		gpCoreForwardFunc = tryAxCoreOutqueueThenFallback
+	}
+	if *gpcore_offload_style == 1 {
+		gpCoreForwardFunc = blockUntilAxcoreAccepts
+	}
+
+	if *axcore_notify_recipient == 0 {
+		axCoreForwardFunc = forwardToCentralizedPostProcThreePhase
+	}
+	if *axcore_notify_recipient == 1 {
+		axCoreForwardFunc = forwardToCentralizedPreProcThreePhase
+	}
+	if *axcore_notify_recipient == 2 {
+		axCoreForwardFunc = forwardToOffloaderThreePhase
+	}
+
+	if *gpcore_input_queue_selector == 0 {
+		gpCoreQueueChooseFunc = firstNonEmptyQueue
+	}
+
+	if *topo == 5 {
+		multi_gpcore_multi_axcore_three_phase(
+			*duration,
+			*speedup,
+			*num_cores,
+			*num_accelerators,
+			*bufferSize,
+			*lambda,
+			*mu,
+			*genType,
+			*phase_one_ratio,
+			*phase_two_ratio,
+			*phase_three_ratio,
+			axCoreForwardFunc,
+			gpCoreForwardFunc,
+			gpCoreQueueChooseFunc,
+		)
 	}
 
 }
